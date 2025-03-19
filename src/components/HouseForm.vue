850947edc3b1c7ebf04c393c5e8b783720de954f
Craig Goodman <Craig.7144@gmail.com>
2025-03-19 17:19:08 -0600

    got form

---
+++ src/components/HouseForm.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { ref } from 'vue';
+
+
+
+const editableHouseData = ref({
+  bedrooms:0,
+  bathrooms:0,
+  levels:0,
+  imgUrl:'',
+  year:0,
+  price:0,
+  description:''
+})
+
+
+
+
+</script>
+
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="houseBedrooms">Bedrooms</label>
+      <input
+        id="houseBedrooms"
+        type="number"
+        required
+        max="50"
+        placeholder="Bedrooms..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="houseBathrooms">Bathrooms</label>
+      <input
+        id="houseBathrooms"
+        type="number"
+        required
+        max="50"
+        placeholder="Bathrooms..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="houseLevels">Levels</label>
+      <input
+        id="houseLevels"
+        type="number"
+        required
+        max="50"
+        placeholder="Levels..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="houseImgUrl">Img Url</label>
+      <input
+        id="houseImgUrl"
+        type="url"
+        required
+        maxlength="500"
+        placeholder="Img Url..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="housePrice">Price</label>
+      <input
+        id="housePrice"
+        type="number"
+        required
+        maxlength="50"
+        placeholder="Price..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="houseDescription">Description</label>
+      <input
+        id="houseDescription"
+        type="text"
+        required
+        maxlength="600"
+        placeholder="Description..."
+      />
+    </div>
+    <div>
+      <button type="reset" class="btn btn-indigo me-3">Reset Form</button>
+      <button type="submit" class="btn btn-success">Submit</button>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+
+label{
+  display: block;
+}
+
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import HouseForm from '@/components/HouseForm.vue';
 import HousesListing from '@/components/HousesListing.vue';
 import { housesService } from '@/services/HousesService.js';
 import { logger } from '@/utils/Logger.js';
@@ -41,6 +42,18 @@ async function getHouses(){
 </div>
 <div class="container">
   <div class="row">
+    <div class="col-md-6">
+      <HouseForm/>
+    </div>
+    <div class="col-md-6">
+      <div class="mb-3">
+        <img src="https://images.unsplash.com/photo-1577398304089-20359eb8a7ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODB8fGhvdXNlc3xlbnwwfDF8MHx8fDA%3D" alt="">
+</div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
     <div v-for="house in houses" :key="house.id" class="col-12">
       <HousesListing :houseProp="house"/>
     </div>
@@ -53,5 +66,8 @@ async function getHouses(){
 
 
 <style lang="scss" scoped>
-
+img{
+  height: 65dvh;
+  width: 100%;
+}
 </style>
